dcbba20f59dcb019db8553572ea4be8a5a6c6cd7
Oliver Chen <59662605+ochen1@users.noreply.github.com>
2024-04-01 21:21:34 -0600

    Add sample usage for gfx1010 (need gfx version override)

--- install-gfx1010.sh
+++ install-gfx1010.sh
@@ -1,2 +1,5 @@
 echo "Python <= 3.10 only!"
 uv pip install -r requirements-gfx1010.txt --extra-index-url https://download.pytorch.org/whl/rocm5.2
+echo
+echo "Example usage:"
+echo "HSA_OVERRIDE_GFX_VERSION=10.3.0 python3 insanely-fast-whisper.py --model distil-whisper/distil-medium.en audio.mp3"
